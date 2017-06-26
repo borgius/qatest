62c86d9a9e7bcf1cce7f65f641332096f25bd230
sshadoww <subsanik@gmail.com>
2017-06-27 01:31:50 +0300

    Add files via upload

--- testcases/demo_test.js
+++ testcases/demo_test.js
@@ -1,5 +1,4 @@
-const faker = require('faker'),
-    _ = require('lodash');
+const faker = require('faker');
 
 /* Create fake data for forms */
 let rand = faker.internet.password(8, true);
@@ -8,15 +7,67 @@ let data = {
  pwd : 'qweqwe123',
  firstName: faker.name.firstName(),
  lastName: faker.name.lastName(),
- email : `a-${rand}@mailinator.com`,
- emailGetStarted : `b-${rand}@mailinator.com`,
+ email : `a-${rand}@gmail.com`,
 };
 
 /* Scenarios for "windowSize": "maximize" */
 Feature('Demo');
 
-Scenario('User can create account', (I) => {
-    I.amOnPage(data.page);
-    I.waitForText('Create Account', 80);
-  }
-);
+/*---------CREATE ACCOUNT----------*/
+
+Scenario('Create account', (I, LogoutPage) => {
+
+	I.amOnPage(data.page);
+	I.see('Create Account', '.build-a-garage');
+	I.click('Create Account', '.build-a-garage');
+	I.fillField('#FirstName', data.firstName);
+	I.fillField('#LastName', data.lastName);
+	I.fillField('#Email', data.email);
+	I.fillField('#Password', data.pwd);
+	I.fillField('#ConfirmPassword', data.pwd);
+	I.click('Create My Account', '#createAccountSubmitButton');
+
+	//Find My Car
+
+	I.waitForElement('#text-search-input', 30);
+	I.seeInTitle('Search | Results');
+	I.fillField('#text-search-input', "Volkswagen");
+	I.click('.keyword-search-result-selected-row');
+
+	//logout
+
+	LogoutPage.logout();
+	
+});
+
+/*---------LOGIN AND LOGOUT----------*/
+
+Scenario('Login and logout', (I, LoginPage, LogoutPage) => {
+
+	I.amOnPage(data.page);
+	 
+	//login
+
+	LoginPage.login();
+
+	I.waitForElement('.account-ribbon-bar-text-desktop', 30);
+	I.seeInTitle('My Account');
+
+	//Logout
+	
+	LogoutPage.logout();
+
+});
+
+/*---------Find My Car----------*/
+
+Scenario('Find My Car', (I) => {
+
+	I.amOnPage(data.page);
+	I.waitForElement('#text-search-input', 30);
+	I.fillField('#text-search-input', "Volkswagen");
+	I.click('.filter-result');
+	I.waitForElement('#text-search-input', 30);
+	I.seeInTitle('Search | Results');
+
+});
