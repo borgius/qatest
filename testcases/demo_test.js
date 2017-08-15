3f00897f2290c0d2f05c2e3331de9ea2740a5996
Luchnikov <mrto0od@gmail.com>
2017-08-15 17:39:52 +0300

    Removed the repetition, simplified the test.

--- testcases/demo_test.js
+++ testcases/demo_test.js
@@ -1,6 +1,6 @@
 const faker = require('faker');
 const Factory = require('rosie').Factory;
-const timeout = 40;
+const timeout = 80;
 /* Create fake data for forms */
 
 Factory.define('user')
@@ -17,96 +17,58 @@ var data = Factory.build('user');
 Feature('Demo');
 
 
-Scenario('User create account', (I) => {
+Scenario('Create Account + Logout + Login', (I) => {
 
  	//  Create Account 
 
     I.amOnPage(data.page);
-    I.see ('Create Account');
-	I.click('.link-text');
-	I.waitForElement('#FirstName', timeout);
-	I.click('#FirstName');
-	I.fillField('#FirstName', data.firstName);
-	I.click('#LastName');
-	I.fillField('#LastName', data.lastName);
-	I.click('#Email');
-	I.fillField('#Email', data.email);
-	I.click('#DeliveryZip');
-	I.fillField('#DeliveryZip', data.zip);
-	I.click('#Password');
-	I.fillField('#Password', data.pwd);
-	I.click('#ConfirmPassword');
-	I.fillField('#ConfirmPassword', data.pwd);
-	I.waitForElement('#createAccountSubmitButton', timeout);
-	I.click('#createAccountSubmitButton');
-	I.waitForText(data.firstName, timeout);
-	}
-);
-
-
-Scenario('Login + Logout', (I) => {
-
-	 //  Create Account 
-
-	var data = Factory.build('user');
-
-    I.amOnPage(data.page);
-    I.see ('Create Account');
-	I.click('.link-text');
-	I.waitForElement('#FirstName', timeout);
-	I.click('#FirstName');
-	I.fillField('#FirstName', data.firstName);
-	I.click('#LastName');
-	I.fillField('#LastName', data.lastName);
-	I.click('#Email');
-	I.fillField('#Email', data.email);
-	I.click('#DeliveryZip');
-	I.fillField('#DeliveryZip', data.zip);
-	I.click('#Password');
-	I.fillField('#Password', data.pwd);
-	I.click('#ConfirmPassword');
-	I.fillField('#ConfirmPassword', data.pwd);
-	I.waitForElement('#createAccountSubmitButton', timeout);
-	I.click('#createAccountSubmitButton');
-	I.waitForText(data.firstName, timeout);
+ 	I.waitForText('Create Account', timeout, '.link-text');
+ 	I.click('.link-text');
+	I.waitForElement('#account-create-form', timeout);
+	within('#account-create-form', () => {
+	   	I.fillField('#FirstName', data.firstName);
+	    I.fillField('#LastName', data.lastName);
+		I.fillField('#Email', data.email);
+		I.fillField('#DeliveryZip', data.zip);
+	    I.fillField('#Password', data.pwd);
+	    I.fillField('#ConfirmPassword', data.pwd);
+	    I.click('#createAccountSubmitButton');
+	  });
+	 I.waitForText(`${data.firstName}'s Account`, timeout, '.account-dropdown-txt');
 
 	//  Logout
 
 	I.waitForElement('#account-dropdown-btn', timeout);
 	I.moveCursorTo('#account-dropdown-btn');
-	I.waitForText('Sign Out', timeout);
+	I.waitForText('Sign Out', timeout, '#account-dropdown');
 	I.click('Sign Out');
-
+	
 	//  Login
 
-	I.waitForText('Sign In', timeout);
+	I.waitForText('Sign In', timeout, '.my-account');
 	I.click('Sign In');
-	I.waitForElement('#EmailAddress', timeout);
-	I.click('#EmailAddress');
-	I.fillField('Email', data.email);
-	I.waitForElement('#Password', timeout);
-	I.click('#Password');
-	I.fillField('Password', data.pwd);
-	I.waitForElement('.button.primary-btn', timeout);
-	I.click('.button.primary-btn');
-	I.waitForText(data.firstName, timeout);
-	
+	I.waitForElement('#account-login-form', timeout);
+	within('#account-login-form', () =>{
+		I.fillField('Email', data.email);
+		I.fillField('Password', data.pwd);
+		I.click('.button.primary-btn');
+	 });
+	I.waitForText(`${data.firstName}'s Account`, timeout, '.account-dropdown-txt');
 	}
 );
-
 
 Scenario('Find Volkswagen', (I) => {
 
 	//'Find My Car' for Volkswagen
 	
 	I.amOnPage(data.page);
-	I.waitForText('Find My Car', timeout);
-	I.click('Find My Car');
-	I.waitForElement('#text-search-input',timeout);
-	I.click('#text-search-input');
-	I.fillField('#text-search-input', 'Volkswagen');
-	I.pressKey('Enter');
-	I.waitForText('PASSAT', timeout);
+	I.waitForElement('.text-search-input-buffer',timeout);
+  	within('.text-search-input-buffer', () => {
+  		I.fillField('#text-search-input', 'Volkswagen');
+  		I.waitForText('Volkswagen', timeout, '.text-search-results');
+  		I.click("//span[contains(text(), 'Volkswagen')]");
+	});
+	I.waitForText('VOLKSWAGEN', timeout, '.tag');
 	}
 );
 
@@ -115,14 +77,14 @@ Scenario('Find Honda Civic < $15000', (I) => {
 	//'Find My Car' for Honda Civic
 
 	I.amOnPage(data.page);
-	I.waitForText('Find My Car', timeout);
-	I.click('Find My Car');
-	I.waitForElement('#text-search-input',timeout);
-	I.click('#text-search-input');
-	I.fillField('#text-search-input', 'Honda Civic');
-	I.pressKey('Enter');
-	I.waitForText('$50,000', timeout);
-	I.waitForElement('.rz-bar.rz-selection', timeout); 
+	I.waitForElement('.text-search-input-buffer',timeout);
+  	within('.text-search-input-buffer', () => {
+  		I.fillField('#text-search-input', 'Honda Civic');
+  		I.waitForText('Honda Civic', timeout, '.text-search-results');
+  		I.click("//span[contains(text(), 'Honda Civic')]");
+	});
+	I.waitForText('$50,000', timeout, '.range-max-value');
+	I.waitForElement('.rz-bar.rz-selection', timeout);
 
 	//Create an element to which we will move the cursor.
 
@@ -134,12 +96,11 @@ Scenario('Find Honda Civic < $15000', (I) => {
 		elem1.setAttribute("style", "display:block; position: absolute; left: 36.75px;");
 	});
 	I.dragAndDrop('.rz-pointer.rz-pointer-max', '#elem-goto');
-pause();
+
 	//User can select a vehicle from results
 
 	I.waitForElement(".search-result-tile-image-container", timeout);
 	I.click(".search-result-tile-image-container"); 
-	I.waitForText("Get Started", timeout);
-	
+	I.waitForText("Get Started", timeout, '.vdp-header-actions');
   }
 );
