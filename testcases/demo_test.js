a88080a616a310c978ffb3b619f78c5397585c24
BardinVlad <32225801+BardinVlad@users.noreply.github.com>
2017-10-01 10:39:08 +0300

    Fixed account creation
    Here I fixed account creation

--- testcases/demo_test.js
+++ testcases/demo_test.js
@@ -18,5 +18,19 @@ Feature('Demo');
 Scenario('User can create account', (I) => {
     I.amOnPage(data.page);
     I.waitForText('Create Account', 80);
+    I.click('.build-a-garage');
+    I.waitForElement('#FirstName');
+    I.fillField('#FirstName',data.firstName);
+    I.waitForElement('#LastName');
+    I.fillField('#LastName',data.lastName);
+    I.waitForElement('#Email');
+    I.fillField('#Email', data.email);
+    I.waitForElement('#Password');
+    I.fillField('#Password', data.pwd);
+    I.waitForElement('#ConfirmPassword');
+    I.fillField('#ConfirmPassword', data.pwd);
+    I.waitForElement('#createAccountSubmitButton', 5);
+    I.click('#createAccountSubmitButton');
+    I.waitForElement('.search-content-container' ,10);
   }
 );
