46f096b3938e56ce7ac89e6ff464d234522eb765
BardinVlad <32225801+BardinVlad@users.noreply.github.com>
2017-10-04 21:03:33 +0300

    все пункты
    1) Создать аккаунт
    2)Логин и логаут (вынести в pageObject не вышло ошибка  Could not include object I from module 'C:\Users\User\autoTests\qatest-master\steps_file.js' Unexpected token .)
    3)Поиск "Volkswagen"
    4)Поиск "honda ciciv < 15000$"

--- testcases/demo_test.js
+++ testcases/demo_test.js
@@ -1,79 +1,76 @@
-const faker = require('faker'),
-    _ = require('lodash');
-
-/* Create fake data for forms */
-let rand = faker.internet.password(8, true);
-let data = {
- page: 'https://goo.gl/6rj684',
- pwd : 'qweqwe123',
- firstName: faker.name.firstName(),
- lastName: faker.name.lastName(),
- email : `a-${rand}@mailinator.com`,
- emailGetStarted : `b-${rand}@mailinator.com`,
-};
-
-/* Scenarios for "windowSize": "maximize" */
-Feature('Demo');
-
+    'use strict';
+        const faker = require('faker'),
+        _ = require('lodash');
+    /* Create fake data for forms */
+        let rand = faker.internet.password(8, true);
+        let data = {
+        page: 'https://goo.gl/6rj684',
+        pwd : 'qweqwe123',
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email : `a-${rand}@mailinator.com`,
+        emailGetStarted : `b-${rand}@mailinator.com`,
+    };
+Feature('My test');
 Scenario('User can create account', (I) => {
-    I.amOnPage(data.page);
-    I.waitForText('Create Account', 80);
-    I.click('.build-a-garage');
-    I.waitForElement('#FirstName');
-    I.fillField('#FirstName',data.firstName);
-    I.waitForElement('#LastName');
-    I.fillField('#LastName',data.lastName);
-    I.waitForElement('#Email');
-    I.fillField('#Email', data.email);
-    I.waitForElement('#Password');
-    I.fillField('#Password', data.pwd);
-    I.waitForElement('#ConfirmPassword');
-    I.fillField('#ConfirmPassword', data.pwd);
-    I.waitForElement('#createAccountSubmitButton', 5);
-    I.click('#createAccountSubmitButton');
+        I.amOnPage(data.page);
+        I.waitForElement('.build-a-garage', 30);
+        I.click('.build-a-garage');
+        I.waitForElement('#FirstName');
+        I.fillField('#FirstName',data.firstName);
+        I.waitForElement('#LastName');
+        I.fillField('#LastName',data.lastName);
+        I.waitForElement('#Email');
+        I.fillField('#Email', data.email);
+        I.waitForElement('#Password');
+        I.fillField('#Password', data.pwd);
+        I.waitForElement('#ConfirmPassword');
+        I.fillField('#ConfirmPassword', data.pwd);
+        I.waitForElement('#createAccountSubmitButton', 5);
+        I.click('#createAccountSubmitButton');
   }
 );
-Scenario('User can login/ logout', (I) => {
-        //login
-    I.amOnPage('https://testazure.carvana.com');
+    Scenario('User can login/ logout', (I, steps_file) => {
+            //login
+        I.amOnPage('https://testazure.carvana.com');
     I.click('Sign In', '.my-account');
     I.waitForElement('#EmailAddress', 10);
-    I.fillField('#EmailAddress', 'aaaaaaaaaa@gmail.com');
+    I.fillField('#EmailAddress', email);
     I.waitForElement('#Password', 10);
-    I.fillField('#Password', 'aaaaaaaaaa1');
+    I.fillField('#Password', password);
     I.pressKey('Enter');
-    I.waitForText('WELCOME TO YOUR CARVANA ACCOUNT', 10) //no
-        //logout
-    I.moveCursorTo('#account-dropdown-btn');
-    I.waitForText('Sign Out', 20);
-    I.click('Sign Out');
-    I.waitForText('TO BUY A CAR', 10);
+            //logout
+        I.moveCursorTo('#account-dropdown-btn');
+        I.waitForText('Sign Out', 20);
+        I.click('Sign Out');
+        I.waitForText('TO BUY A CAR', 10);
     }
 );
-Scenario('User can fined VOLKSWAGEN', (I) => {
+    Scenario('User can fined VOLKSWAGEN', (I) => {
         I.amOnPage(data.page);
         I.waitForElement('#text-search-input', 20);
         I.fillField('#text-search-input', 'Volkswagen');
         I.click('.filter-result');
         I.waitForElement('.search-content-container', 20)
     }
-)
- Scenario('User can fined HONDA CIVIC <15k', (I) => {
+);
+    Scenario('User can fined HONDA CIVIC <15k', (I) => {
         I.amOnPage(data.page);
-        I.waitForText('TO BUY A CAR', 10);
-        I.waitForElement('#text-search-input', 20);
-        I.fillField('#text-search-input', 'Honda Civic');
-        I.click('.filter-result');
-        I.waitForElement('.search-content-container', 20); // мы узнаем что результаты появились на страничке
-        I.waitForText('Honda Civic', 5); // мы точно будем знать что нам нашло именно Honda Civic
-        //функция передвигаюшия слайдер
-    I.executeScript(function() {
-                let SpanP = document.querySelector(".rzslider .rz-bar .rz-selection");
-                let element = document.createElement("div");
-                SpanP.appendChild(elem);
-        		elem.setAttribute("id","elem-goto");
-        		elem.setAttribute("style", "display:block; position: absolute; left: 36.75px;");
-        });
-    	I.dragAndDrop('.rz-pointer.rz-pointer-max', '#elem-goto');
-    	I.waitForText('$1000 - $15000', timeout, '.tag');
-    }
+    I.waitForText('TO BUY A CAR', 10);
+    I.waitForElement('#text-search-input', 20);
+    I.fillField('#text-search-input', 'Honda Civic');
+    I.click('.filter-result', 'Honda Civic');
+    I.waitForElement('.search-content-container', 20); // мы узнаем что результаты появились на страничке
+    I.waitForText('Honda Civic', 20); // мы точно будем знать что нам нашло именно Honda Civic
+    //функция передвигаюшия слайдер
+    I.executeScript(function () {
+        var spanCopy = document.querySelector(".rzslider .rz-bar.rz-selection")
+        var element =document.createElement("div");
+        spanCopy.appendChild(element);
+        element.setAttribute("id", "newPlaceForSlider");
+        element.setAttribute("style", "display:block; position:absolute; left: 36.75px")
+    });
+    I.dragAndDrop('.rz-pointer.rz-pointer-max', '#newPlaceForSlider');
+    I.waitForText('HONDA CIVIC', 20);
+    I.waitForText('$1000 - $15000', 20);
+});
