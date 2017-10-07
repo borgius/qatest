1d4cd8eb36f2c7279708eb4065462c7a283121e2
BardinVlad <32225801+BardinVlad@users.noreply.github.com>
2017-10-07 16:21:17 +0300

    Update demo_test.js

--- testcases/demo_test.js
+++ testcases/demo_test.js
@@ -1,76 +1,64 @@
-    'use strict';
-        const faker = require('faker'),
-        _ = require('lodash');
-    /* Create fake data for forms */
-        let rand = faker.internet.password(8, true);
-        let data = {
-        page: 'https://goo.gl/6rj684',
-        pwd : 'qweqwe123',
-        firstName: faker.name.firstName(),
-        lastName: faker.name.lastName(),
-        email : `a-${rand}@mailinator.com`,
-        emailGetStarted : `b-${rand}@mailinator.com`,
-    };
+'use strict';
+const faker = require('faker'),
+    _ = require('lodash');
+/* Create fake data for forms */
+let rand = faker.internet.password(8, true);
+let data = {
+    page: 'https://goo.gl/6rj684',
+    pwd : 'qweqwe123',
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    email : `a-${rand}@mailinator.com`,
+    emailGetStarted : `b-${rand}@mailinator.com`,
+};
 Feature('My test');
 Scenario('User can create account', (I) => {
-        I.amOnPage(data.page);
-        I.waitForElement('.build-a-garage', 30);
-        I.click('.build-a-garage');
-        I.waitForElement('#FirstName');
-        I.fillField('#FirstName',data.firstName);
-        I.waitForElement('#LastName');
-        I.fillField('#LastName',data.lastName);
-        I.waitForElement('#Email');
-        I.fillField('#Email', data.email);
-        I.waitForElement('#Password');
-        I.fillField('#Password', data.pwd);
-        I.waitForElement('#ConfirmPassword');
-        I.fillField('#ConfirmPassword', data.pwd);
-        I.waitForElement('#createAccountSubmitButton', 5);
-        I.click('#createAccountSubmitButton');
-  }
-);
-    Scenario('User can login/ logout', (I) => {
-            //login
-        I.amOnPage('https://testazure.carvana.com');
-    I.click('Sign In', '.my-account');
-    within('#account-login-form', () => {
-    I.fillField('Email', data.email);
-    I.fillField('Password', data.pwd);
+    I.amOnPage(data.page);
+I.waitForElement('.build-a-garage', 30);
+I.click('.build-a-garage');
+I.waitForElement('#FirstName');
+I.fillField('#FirstName',data.firstName);
+I.waitForElement('#LastName');
+I.fillField('#LastName',data.lastName);
+I.waitForElement('#Email');
+I.fillField('#Email', data.email);
+I.waitForElement('#Password');
+I.fillField('#Password', data.pwd);
+I.waitForElement('#ConfirmPassword');
+I.fillField('#ConfirmPassword', data.pwd);
+I.waitForElement('#createAccountSubmitButton', 5);
+I.click('#createAccountSubmitButton');
 });
-    I.pressKey('Enter');
-            //logout
-       I.waitForText(data.email + 'Account');
-        I.moveCursorTo(data.email + 'Account');
-        I.waitForText('Sign Out', 20);
-        I.click('Sign Out');
-        I.seeInCurentUrl('https://testazure.carvana.com');
-    }
-);
-    Scenario('User can fined VOLKSWAGEN', (I) => {
-        I.amOnPage(data.page);
-        I.waitForElement('#text-search-input', 20);
-        I.fillField('#text-search-input', 'Volkswagen');        I.click('.filter-result');
-        I.waitForElement('.search-content-container', 20)
-    }
-);
-    Scenario('User can fined HONDA CIVIC <15k', (I) => {
-        I.amOnPage(data.page);
-    I.waitForText('TO BUY A CAR', 10);
-    I.waitForElement('#text-search-input', 20);
-    I.fillField('#text-search-input', 'Honda Civic');
-    I.click('.filter-result');
-    I.waitForElement('.search-content-container', 20); // мы узнаем что результаты появились на страничке
-    I.waitForText('Honda Civic', 20); // мы точно будем знать что нам нашло именно Honda Civic
-    //функция передвигаюшия слайдер
-    I.executeScript(function () {
-        var spanCopy = document.querySelector(".rzslider .rz-bar.rz-selection")
-        var element =document.createElement("div");
-        spanCopy.appendChild(element);
-        element.setAttribute("id", "newPlaceForSlider");
-        element.setAttribute("style", "display:block; position:absolute; left: 36.75px")
-    });
-    I.dragAndDrop('.rz-pointer.rz-pointer-max', '#newPlaceForSlider');
-    I.waitForText('HONDA CIVIC', 20);
-    I.waitForText('$1000 - $15000', 20);
+Scenario('User can login/ logout', (I, steps_file) => {
+    //login
+    I.login();
+    //logout
+    I.logout();
+});
+Scenario('User can fined VOLKSWAGEN', (I) => {
+    I.amOnPage(data.page);
+I.waitForElement('#text-search-input', 20);
+I.fillField('#text-search-input', 'Volkswagen');
+I.click('.filter-result');
+I.waitForElement('.search-content-container', 20)
+});
+Scenario('User can fined HONDA CIVIC <15k', (I) => {
+    I.amOnPage(data.page);
+I.waitForText('TO BUY A CAR', 10);
+I.waitForElement('#text-search-input', 20);
+I.fillField('#text-search-input', 'Honda Civic');
+I.click('.filter-result');
+I.waitForElement('.search-content-container', 20); // мы узнаем что результаты появились на страничке
+I.waitForText('Honda Civic', 20); // мы точно будем знать что нам нашло именно Honda Civic
+//функция передвигаюшия слайдер
+I.executeScript(function () {
+    var spanCopy = document.querySelector(".rzslider .rz-bar.rz-selection")
+    var element =document.createElement("div");
+    spanCopy.appendChild(element);
+    element.setAttribute("id", "newPlaceForSlider");
+    element.setAttribute("style", "display:block; position:absolute; left: 36.75px")
+});
+I.dragAndDrop('.rz-pointer.rz-pointer-max', '#newPlaceForSlider');
+I.waitForText('HONDA CIVIC', 20);
+I.waitForText('$1000 - $15000', 20);
 });
